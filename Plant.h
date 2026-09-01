a615cf239ad647f0f4ef98ff5a51693b20a2d221
MimiVG24 <mimigarneva@gmail.com>
2026-09-02 00:06:30 +0300

    Enhance Plant class with constructors and type enum
    Added constructors, copy constructor, and assignment operator for the Plant class. Introduced an enum for plant types.

--- Plant.h
+++ Plant.h
@@ -2,14 +2,44 @@
 #define PLANT_H
 
 #pragma once
+#include <iostream>
+
+enum Type {
+    Grass = 16,
+    Tree = 8, 
+    Bush = 4, 
+    Moss = 2, 
+    Other = 1
+};
 
 class Plant
 {
 public:
-    Plant();
-    ~Plant();
+    Plant(const char* p, const char* s, const unsigned int h, const Type t);
+
+    Plant(const Plant& other); 
+    Plant& operator=(const Plant& other);
+
+    const char* getProduce() const;
+    const char* getSort() const;
+    const unsigned int getHeight() const;
+    const Type getType() const;
+    void setProduce(const char* produce);
+    void setSort(const char* sort);
+    void setHeight(unsigned int h);
+
+    virtual Plant* clone() const = 0;
+    virtual char* description() const = 0;
+
+    virtual ~Plant();
+protected:
+    char* produce = nullptr;
+    char* sort = nullptr;
+    unsigned int height;
 
 private:
+    Type type;
+    virtual bool typePred(const Type& t) const;
 
 };
 
