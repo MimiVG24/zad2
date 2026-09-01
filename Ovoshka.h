a39708b299c067d12d71a97903ca802cc6033379
MimiVG24 <mimigarneva@gmail.com>
2026-09-02 00:07:26 +0300

    Enhance Ovoshka class with constructors and methods
    Added constructors, assignment operator, and methods for recipe management in Ovoshka class.

--- Ovoshka.h
+++ Ovoshka.h
@@ -2,15 +2,27 @@
 #define OVOSHKA_H
 
 #pragma once
+#include "Plant.h"
 
-class Ovoshka
+class Ovoshka : public Plant
 {
 public:
-    Ovoshka();
+    Ovoshka(const char* p, const char* s, const unsigned int h, const Type t, const char* rec);
+
+    Ovoshka(const Ovoshka& other);
+    Ovoshka& operator=(const Ovoshka& other);
+
+    const char* getRecepie() const;
+    void setRecepie(const char* rec);
+
+    virtual Plant* clone() const override;
+    virtual char* description() const override;
+
     ~Ovoshka();
 
 private:
-
+    char* recepie = nullptr;
+    virtual bool typePred(const Type& t) const override;
 };
 
 #endif
