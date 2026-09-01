de16aa46b18e0b13bb5f6ecf25525f3d7426d079
MimiVG24 <mimigarneva@gmail.com>
2026-09-02 00:09:09 +0300

    Change months array to inline and update constructor

--- Flower.h
+++ Flower.h
@@ -4,19 +4,24 @@
 #pragma once
 #include "Plant.h"
 
-static char* months[12] = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+inline char* months[12] = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 class Flower : public Plant
 {
 public:
-    Flower(const char* p, const char* s, const unsigned int h, const Type t, unsigned int* g);
+    Flower(const char* p, const char* s, const unsigned int h, const Type t, const unsigned int* g);
+
+    Flower(const Flower& other);
+    Flower& operator=(const Flower& other);
+
+    const unsigned int* getGrowingMonths() const;
+    void  setGrowingMonths(const unsigned int* months);
     virtual Plant* clone() const override;
     virtual char* description() const override;
     ~Flower();
 
 private:
     unsigned int growingMonths[12]{};
-    virtual bool typePred(const Type& t) const override;
 };
 
 #endif
