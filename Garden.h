8d85a32c11713aa7aa62c5244c00d77453ae382b
MimiVG24 <mimigarneva@gmail.com>
2026-09-02 00:08:07 +0300

    Update Garden.h

--- Garden.h
+++ Garden.h
@@ -2,15 +2,21 @@
 #define GARDEN_H
 
 #pragma once
+#include "Plant.h"
 
 class Garden
 {
 public:
-    Garden();
+    Garden(size_t cap);
+    void plant(const Plant& p);
+    void eradicate(const Type& type);
+    void info() const;
     ~Garden();
 
 private:
-
+    Plant** plants;
+    size_t capacity;
+    size_t count;
 };
 
 #endif
